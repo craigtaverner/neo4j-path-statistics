710b52f9fe06333fac2645c167868d7843cd205c
realyasswl <realyasswl@126.com>
2017-03-04 18:53:33 +0100

    add label path statistics procedure

--- src/main/java/org/amanzi/neo4j/paths/PathStatisticsProcedures.java
+++ src/main/java/org/amanzi/neo4j/paths/PathStatisticsProcedures.java
@@ -2,20 +2,28 @@ package org.amanzi.neo4j.paths;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.kernel.api.exceptions.ProcedureException;
 import org.neo4j.procedure.*;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.neo4j.logging.Log;
+import org.neo4j.kernel.impl.logging.LogService;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
 /**
  * Sample procedures, provided here as a template. Also used in test code.
  */
-public class PathStatisticsProcedures
-{
+public class PathStatisticsProcedures {
     @Context
     public GraphDatabaseService graph;
 
@@ -39,14 +47,53 @@ public class PathStatisticsProcedures
                 depth -> new LengthResult( depth, results.containsKey( depth ) ? results.get( depth ) : 0 ) );
     }
 
-    public static class LengthResult
-    {
+    @Description("Produce a table of label paths and number of paths found for each length")
+    @Procedure("labelpath.stats")
+    public Stream<LabelResult> getLabelPathStats(@Name(value = "maxDepth", defaultValue = "3") long maxDepth) throws ProcedureException {
+        LinkedHashMap<String, Long> results = new LinkedHashMap<>();
+        LogService logService = ((GraphDatabaseAPI) graph).getDependencyResolver().resolveDependency(LogService.class);
+        Log log = logService.getUserLog(getClass());
+        graph.getAllNodes().stream().forEach(node ->
+        {
+            TraversalDescription td =
+                    graph.traversalDescription().depthFirst().evaluator(Evaluators.toDepth((int) maxDepth));
+            for (Path path : td.traverse(node)) {
+                StringBuilder sb = new StringBuilder();
+                Iterator<Relationship> iter = path.relationships().iterator();
+                while (iter.hasNext()) {
+                    Relationship rs = iter.next();
+                    sb.append(rs.getProperty("label")).append(",");
+                }
+
+                int length = path.length();
+                if (length > 0) {
+                    sb.setLength(sb.length() - 1);
+                }
+                String pathLabel = sb.toString();
+                Long count = results.get(pathLabel);
+
+                results.put(pathLabel, count == null ? 1 : count + 1);
+            }
+        });
+        return results.entrySet().stream().map(e -> new LabelResult(e.getKey(), e.getValue()));
+    }
+
+    public static class LengthResult {
         public long length;
         public long value;
 
-        public LengthResult( long length, long value )
-        {
+        public LengthResult(long length, long value) {
             this.length = length;
+            this.value = value;
+        }
+    }
+
+    public static class LabelResult {
+        public String label;
+        public long value;
+
+        public LabelResult(String label, long value) {
+            this.label = label;
             this.value = value;
         }
     }
---
+++ src/test/java/org/amanzi/neo4j/paths/PathLabelStatisticsProceduresTest.java
@@ -0,0 +1,80 @@
+package org.amanzi.neo4j.paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.kernel.impl.proc.Procedures;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.amanzi.neo4j.paths.TestUtil.testCallCount;
+import static org.amanzi.neo4j.paths.TestUtil.testResult;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class PathLabelStatisticsProceduresTest {
+
+    private GraphDatabaseService db;
+
+
+    private String graphDbDir = "/home/liwang/neo4j/neo4j-community-3.1.1/data/databases";
+    private String pathToConfig = "/home/liwang/neo4j/neo4j-community-3.1.1/conf/";
+
+    @Before
+    public void setUp() throws Exception {
+//        db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        db = new GraphDatabaseFactory().newEmbeddedDatabase(new File(graphDbDir + "/health"));
+        ((GraphDatabaseAPI) db).getDependencyResolver().resolveDependency(Procedures.class)
+                .registerProcedure(PathStatisticsProcedures.class);
+//        try (Transaction tx = db.beginTx()) {
+//            Label label = Label.label("User");
+//            RelationshipType relType = RelationshipType.withName("KNOWS");
+//            Node root = db.createNode(label);
+//            for (int depth = 1; depth < 10; depth++) {
+//                Node prev = root;
+//                for (int i = 0; i < depth; i++) {
+//                    Node child = db.createNode(label);
+//                    prev.createRelationshipTo(child, relType);
+//                    prev = child;
+//                }
+//            }
+//            tx.success();
+//        }
+    }
+
+    @After
+    public void tearDown() {
+        db.shutdown();
+    }
+
+    @Test
+    public void testCountPathsDepthTwo() {
+        long maxDepth = 2;
+        testCallCount(db, "CALL labelpath.stats(" + maxDepth + ")", null, 43);
+        testResult(db, "CALL labelpath.stats(" + maxDepth + ")", result ->
+        {
+            while (result.hasNext()) {
+                Map<String, Object> row = result.next();
+                System.out.println(row);
+                assert (row.containsKey("label"));
+                assert (row.containsKey("value"));
+                String label = (String) row.get("label");
+                long count = (Long) row.get("value");
+//                assertThat(length, greaterThanOrEqualTo(0L));
+//                assertThat(length, lessThan(maxDepth));
+//                assertThat(count, greaterThan(0L));
+            }
+        });
+    }
+
+    @Test
+    public void testCountPathsDefaultDepth() {
+        testCallCount(db, "CALL labelpath.stats", null, 259);
+    }
+}
